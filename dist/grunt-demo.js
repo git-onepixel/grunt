90267fea448a2bc685757cb0e97cb19b2324c962
git-onepixel <onepixel@126.com>
2016-01-10 16:46:30 +0800

    update

--- dist/grunt-demo.js
+++ dist/grunt-demo.js
@@ -15,7 +15,9 @@ var b = (function(){
     return{
         print:function(first,second){
             var result = a.add(first,second);
-            return result;
+
+            return result; 
+			
         }
     }
 })();
